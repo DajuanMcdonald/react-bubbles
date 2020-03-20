07df4d45d8bb7b143529daeba6b592cc0d24670b
Dajuan Mcdonald <flopwiki@gmail.com>
2020-03-20 19:36:19 -0400

    added login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -10,6 +10,16 @@ const Login = () => {
       <h1>Welcome to the Bubble App!</h1>
       <div className="login-form">
       <p>Build a login page here</p>
+      <form onChange={() => console.log('form updated....')}>
+        <label>Username: </label>
+        <input type="text" name="username"/>
+
+        <label>Password</label>
+        <input type="password" name="password"/>
+
+        <button onClick={(e) => { e.preventDefault(); console.log('button updated....')}}>Log In</button>
+
+      </form>
 
       </div>
     </>
