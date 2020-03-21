665883d3986ba29f1252fcb2d13633139c0776dc
Dajuan Mcdonald <flopwiki@gmail.com>
2020-03-20 22:06:53 -0400

    added api call to save

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/AxiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -22,6 +23,9 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth().put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res =>{ setAddColor(res.data)})
+    .catch(err => console.log(err.response))
   };
 
   const deleteColor = color => {
