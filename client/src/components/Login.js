56bb8b0f50115f1c4f50f9048d00147aa638bf3f
Dajuan Mcdonald <flopwiki@gmail.com>
2020-03-20 20:32:52 -0400

    fixed login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,29 +1,64 @@
-import React from "react";
-import './styles/login.css'
+import React from 'react'
+import {Link } from 'react-router-dom'
 
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props)
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <div className="login-form">
-      <p>Build a login page here</p>
-      <form onChange={() => console.log('form updated....')}>
-        <label>Username: </label>
-        <input type="text" name="username"/>
+    // always bailout with rest
+    // this.props.logout();
+    this.state = {
+      username: '',
+      password: '',
+      isSubmitting: false
+    }
 
-        <label>Password</label>
-        <input type="password" name="password"/>
+  }
 
-        <button onClick={(e) => { e.preventDefault(); console.log('button updated....')}}>Log In</button>
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value})
+  }
 
-      </form>
+  handleSubmit = e => {
+    e.preventDefault();
 
+    this.setState({ isSubmitting: true });
+    const {username, password} = this.state;
+    if (username && password) {
+      this.props.logIn(username, password);
+    }
+  }
+
+  render() {
+    const { logIn } = this.props;
+    const { username, password, isSubmitting} = this.state;
+
+    return (
+      <div className="col-md-6 col-md-offset-3">
+        <h1>Login</h1>
+        <form name="form" onSubmit={this.handleSubmit}>
+          <div className={'form-group' + (isSubmitting && !username ? 'has-error' : '')}>
+            <label>Username</label>
+            <input type="text" name="username" value={username} onChange={this.handleChange} />
+            {isSubmitting && !username && <div>Username required</div>}
+          </div>
+          <div className={'form-group' + (isSubmitting && !password ? 'had-error' : '')}>
+            <label>Password</label>
+            <input type="password" name="password" value={password} onChange={this.handleChange} />
+            {isSubmitting && !password && <div>Password required</div>}
+
+          </div>
+
+          <div className="form-group">
+            <button className="btn btn-primary">Login</button>
+            <Link to="/" className="btn btn-link">Register</Link>
+
+          </div>
+
+        </form>
+        
       </div>
-    </>
-  );
-};
-
-export default Login;
+    )
+  }
+}
